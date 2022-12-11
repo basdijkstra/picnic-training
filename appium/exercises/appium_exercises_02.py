311aa753477065c89450fae02fa81b247f113e82
Bas Dijkstra <bas@ontestautomation.com>
2022-12-11 08:46:56 +0100

    Add second Appium exercise: interacting with elements

---
+++ appium/exercises/appium_exercises_02.py
@@ -0,0 +1,58 @@
+import pytest
+from appium import webdriver
+from appium.webdriver.common.appiumby import AppiumBy
+from selenium.webdriver.common.by import By
+
+# Exercise 2.1
+# Copy the required settings from the previous exercise into this fixture
+
+@pytest.fixture
+def driver():
+    caps = {
+        # COPY FROM THE PREVIOUS EXERCISE
+    }
+
+    url = 'COPY FROM THE PREVIOUS EXERCISE'
+    driver = webdriver.Remote(url, caps)
+    yield driver
+    driver.quit()
+
+# Exercise 2.2
+# Complete the test by completing all the steps in the test
+# For now, it's OK to put time.sleep(5) between steps to make sure
+# that the app loading times are dealt with. We'll work on that soon :)
+
+def test_purchase_two_backpacks(driver):
+
+    # DONE: Go to the backpack item details
+    xpath_backpack = '(//android.view.ViewGroup[@content-desc="store item"])[1]/android.view.ViewGroup[1]/android.widget.ImageView'
+    driver.find_element(By.XPATH, xpath_backpack).click()
+
+    # DONE: We want to order two backpacks, so tap the '+' button
+    driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'counter plus button').click()
+
+    # DONE: Add our two backpacks to the cart
+    driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'Add To Cart button').click()
+
+    # TO DO: Can you find out how to click the device 'Back' button?
+
+    # TO DO: Check that the number of items in the cart is equal to 2
+    xpath_number_of_items_in_cart = '//android.view.ViewGroup[@content-desc="cart badge"]/android.widget.TextView'
+
+    # TO DO: Click on the shopping cart icon to start the checkout process
+    # a11y ID: cart badge
+
+    # TO DO: Click on the Proceed to checkout button
+    # a11y ID: Proceed To Checkout button
+
+    # TO DO: fill in username bob@example.com
+    # a11y ID: Username input field
+
+    # TO DO: fill in password 10203040
+    # a11y ID: Password input field
+
+    # TO DO: Click on the Login button
+    # a11y ID: Login button
+
+    # TO DO: assert that we're on the checkout screen
+    # a11y ID: checkout address screen
